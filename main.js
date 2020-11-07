2a99f7ed48799f1cb3f11c82fd2892e0e2ad4423
MaryWenzel <wenzel.mary1993@gmail.com>
2020-11-07 15:28:42 -0600

    initial commit

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ const rl = readline.createInterface({
 });
 
 let board = [];
-let solution = '';
+let solution = 'abcd';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let hint;
 
-const printBoard = () =>  {
+
+const printBoard = (guess) =>  {
+  board.push(`Guess is ${guess} and hint is ${hint}`)
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
 }
 
+// secret code 
 const generateSolution = () =>  {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
@@ -28,20 +32,44 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
   // your code here
+  let guessArray = guess.split('');
+  let solutionArray = solution.split('');
+  let correctLetterLocations = 0;
+  let correctLetters = 0;
+  for (let i = 0; i < solutionArray.length; i++){
+    if (guessArray[i] == solutionArray[i]){
+      correctLetterLocations++
+      solutionArray[i] = null
+    }
+  }
+      for (let x = 0; x < solutionArray.length; x++) {
+        let targetIndex = solutionArray.indexOf(guessArray[x])
+        console.log(targetIndex)
+        console.log(guessArray)
+        if (targetIndex > -1) {
+          correctLetters++
+          solutionArray[targetIndex] = null
+        }
+      }
+  return `You have ${correctLetterLocations} in the correct location and ${correctLetters} correct letters`
 }
 
 const mastermind = (guess) => {
   solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
+  if (guess === solution){
+    return 'You guessed it!'
+  }
+  hint = generateHint(guess)
 }
 
 
 const getPrompt = () =>  {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
-    printBoard();
+    printBoard(guess);
     getPrompt();
   });
 }
